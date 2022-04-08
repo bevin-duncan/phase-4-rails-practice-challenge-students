e3db793d721029968eeab8363fef7dda8e7815e4
Bevin Duncan <bevin.duncan@gmail.com>
2022-04-08 07:56:10 -0700

    Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable
+
+    def render_not_found(invalid)
+        render json: {errors: "not found"}, status: :not_found
+    end
+
+    def render_unprocessable(invalid)
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+        
+    end
 end
